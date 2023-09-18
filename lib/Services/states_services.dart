7a5bda74e2dc6fc68b0d792d18489815534cf0ee
Muhammad Hamza <hamzamee2020@gmail.com>
2023-09-18 09:48:18 +0500

    This is my first commit

---
+++ lib/Services/states_services.dart
@@ -0,0 +1,32 @@
+import 'dart:convert';
+
+import 'package:covid_app/Model/WorldStatesModel.dart';
+import 'package:covid_app/Utilities/app_url.dart';
+import 'package:http/http.dart'as http;
+
+class StatesServices {
+
+  Future<WorldStatesModel> fetchWorldStatesRecords()async {
+    final response= await http.get(Uri.parse(AppUrl.worldStatesApi));
+
+    if(response.statusCode == 200){
+      var data = jsonDecode(response.body);
+      return WorldStatesModel.fromJson(data);
+    }else{
+      throw Exception('Error');
+    }
+  }
+
+
+  Future<List<dynamic>> countriesListApi()async {
+
+    final response= await http.get(Uri.parse(AppUrl.countriesList));
+    if(response.statusCode == 200){
+      var data = jsonDecode(response.body);
+      return data;
+    }else{
+      throw Exception('Error');
+    }
+  }
+
+}
---
+++ lib/Utilities/app_url.dart
@@ -0,0 +1,9 @@
+class AppUrl{
+  // this is our base url
+  static const String baseurl = 'https://disease.sh/v3/covid-19/';
+
+  // fetch world covid states
+  static const String worldStatesApi = baseurl + 'all';
+  static const String countriesList = baseurl + 'countries';
+
+}
